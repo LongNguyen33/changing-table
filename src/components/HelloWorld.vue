1b2eb74d24f7c11f1840c0be6276280a654c36da
L030301 <nguyenhailongtlu@gmail.com>
2023-05-08 23:34:26 +0700

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,24 +12,49 @@
           <span>{{ $index + 1 }}</span>
         </template>
       </el-table-column>
-      <el-table-column prop="date" label="Date" width="180"> </el-table-column>
-      <el-table-column prop="name" label="Name" width="180"> </el-table-column>
+      <el-table-column prop="date" label="Date" width="100"> </el-table-column>
+      <el-table-column prop="name" label="Name" width="150"> </el-table-column>
+      <el-table-column prop="status" label="Status" width="150">
+      </el-table-column>
       <el-table-column prop="address" label="Address"> </el-table-column>
     </el-table>
+
     <el-dialog v-if="showForm" title="Sort Form" :visible.sync="showForm">
+      <div style="text-align: left; margin-bottom: 5px">
+        <span>
+          <i class="square-icon-re"></i>
+          Realesed Station
+        </span>
+        <span>
+          <i class="square-icon-dr"></i>
+          Drafted Station
+        </span>
+      </div>
       <div>
-        <el-checkbox-group v-model="checkboxGroup1">
-          <el-checkbox-button v-for="name in names" :label="name" :key="name">{{
-            name
-          }}</el-checkbox-button>
-        </el-checkbox-group>
-        <span>Tuyến đường hiện tại: {{ names.join(" -> ") }}</span>
-        <br />
-        <span>Tuyến đường thay đổi: {{ checkboxGroup1.join(" -> ") }}</span>
+        <div class="button-container">
+          <template v-for="(item, index) in nameAndStatus">
+            <el-button
+              v-if="item.show"
+              :key="index"
+              v-bind:type="item.status === 'Realese' ? 'primary' : 'danger'"
+              @click="addToChooseName(item)"
+            >
+              {{ item.name }}
+            </el-button>
+          </template>
+        </div>
+
+        <el-steps direction="vertical" :active="1">
+          <el-step
+            v-for="(item, index) in chooseName"
+            :key="index"
+            :title="item"
+          ></el-step>
+        </el-steps>
       </div>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="showForm = false">Cancel</el-button>
-        <el-button type="primary" @click="sortTable">Sort</el-button>
+        <el-button @click="cancelSort" type="danger">Cancel</el-button>
+        <el-button type="success" @click="sortTable">Sort</el-button>
       </span>
     </el-dialog>
   </div>
@@ -41,54 +66,91 @@ export default {
   data() {
     return {
       showForm: false,
+      chooseName: [],
       tableData: [
         {
           date: "2016-05-03",
           name: "Circle K",
           address: "No. 189, Grove St, Los Angeles",
+          status: "Realese",
         },
         {
           date: "2016-05-02",
           name: "Starbuck",
           address: "No. 189, Grove St, Los Angeles",
+          status: "Realese",
         },
         {
           date: "2016-05-04",
           name: "Charging Station",
           address: "No. 189, Grove St, Los Angeles",
+          status: "Realese",
         },
         {
           date: "2016-05-01",
           name: "Phenikaa-X",
           address: "No. 189, Grove St, Los Angeles",
+          status: "Realese",
+        },
+        {
+          date: "2016-05-03",
+          name: "Vincom Center",
+          address: "No. 189, Grove St, Los Angeles",
+          status: "Draft",
+        },
+        {
+          date: "2016-05-02",
+          name: "Highland Coffee",
+          address: "No. 189, Grove St, Los Angeles",
+          status: "Draft",
+        },
+        {
+          date: "2016-05-04",
+          name: "Phenikaa School",
+          address: "No. 189, Grove St, Los Angeles",
+          status: "Draft",
+        },
+        {
+          date: "2016-05-01",
+          name: "Phenikaa Hospital",
+          address: "No. 189, Grove St, Los Angeles",
+          status: "Draft",
         },
       ],
-      checkboxGroup1: [],
     };
   },
   computed: {
-    names() {
-      return this.tableData.map((item) => item.name);
+    nameAndStatus() {
+      return this.tableData.map(({ name, status }) => ({
+        name,
+        status,
+        show: true,
+      }));
     },
   },
   methods: {
-    sortTable() {
-      const filteredData = this.tableData.filter((item) =>
-        this.checkboxGroup1.includes(item.name)
-      );
-      filteredData.sort((a, b) => {
-        return (
-          this.checkboxGroup1.indexOf(a.name) -
-          this.checkboxGroup1.indexOf(b.name)
-        );
+    addToChooseName(item) {
+      this.chooseName.push(item.name);
+      item.show = false;
+    },
+    cancelSort() {
+      this.nameAndStatus.forEach((item) => {
+        item.show = true;
       });
-      this.tableData = filteredData.concat(
-        this.tableData.filter(
-          (item) => !this.checkboxGroup1.includes(item.name)
-        )
-      );
+      this.chooseName = [];
       this.showForm = false;
-      this.checkboxGroup1 = [];
+    },
+    sortTable() {
+      const sortedData = [];
+      for (const name of this.chooseName) {
+        const index = this.tableData.findIndex((item) => item.name === name);
+        if (index !== -1) {
+          sortedData.push(this.tableData[index]);
+        }
+      }
+      this.tableData = sortedData;
+      this.chooseName = [];
+      this.showForm = false;
       this.$notify({
         title: "Success",
         message: "Changing route successfully!",
@@ -99,4 +161,51 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@media (max-width: 768px) {
+  .el-dialog__wrapper {
+    width: 100% !important;
+    height: 100% !important;
+    top: 0 !important;
+    left: 0 !important;
+    margin: 0 !important;
+    border-radius: 0 !important;
+  }
+  .el-dialog {
+    width: 100% !important;
+    height: 100% !important;
+    margin: 0 !important;
+    border-radius: 0 !important;
+  }
+  .el-dialog__body {
+    height: calc(100% - 65px) !important;
+    overflow-y: scroll !important;
+  }
+}
+.el-dialog__body .button-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+.el-dialog__body .button-container button {
+  margin: 5px 0;
+  width: 50%;
+  text-align: left;
+}
+.square-icon-re {
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  margin-right: 5px;
+  background-color: #3c8dbc;
+}
+.square-icon-dr {
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  margin-right: 5px;
+  background-color: #e11818;
+}
+</style>
